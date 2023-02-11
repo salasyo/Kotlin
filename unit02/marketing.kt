7174ad5d400b1fb075e94824e267ff932b09bff2
obed <122159877+salasyo@users.noreply.github.com>
2023-02-11 17:23:21 -0600

    Create marketing.kt

---
+++ unit02/marketing.kt
@@ -0,0 +1,13 @@
+fun main() {
+    
+    var friendsCount = 0
+    
+    
+    if friendsCount > 500 
+    
+    println("Adopt a Dog Today!")
+    
+    else
+    
+    println("Buy Cat Food At 20% Off")
+}
