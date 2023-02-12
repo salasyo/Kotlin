8d48baf762afc029d57c98ada428e2b85a0ec2ee
obed <122159877+salasyo@users.noreply.github.com>
2023-02-11 19:07:40 -0600

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    var name: String = "Obed Salas"
+    var age: Int = 19
+    var annualPay: Double = 250000.00
+
+    print("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year." )
+
+}
