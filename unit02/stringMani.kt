592ff691ccf573b14a238a6b5ac1c8695bcf2eda
obed <122159877+salasyo@users.noreply.github.com>
2023-02-11 20:52:29 -0600

    Create stringMani.kt

---
+++ unit02/stringMani.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    println("Enter the name of your favorite city: ")
+    val cityName: String = readLine()
+
+    println(cityName.length)
+    println(cityName.toUpperCase())
+    println(cityName.toLowerCase())
+    println(cityName[0])
+}
