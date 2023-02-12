28774dcfe071a3536cb17b9baa18e55305260d56
obed <122159877+salasyo@users.noreply.github.com>
2023-02-11 18:38:43 -0600

    Update marketing.kt

--- unit02/marketing.kt
+++ unit02/marketing.kt
@@ -1,13 +1,15 @@
 fun main() {
-    
+
     var friendsCount = 0
-    
-    
-    if friendsCount > 500 
-    
-    println("Adopt a Dog Today!")
-    
-    else
-    
-    println("Buy Cat Food At 20% Off")
+
+
+    if (friendsCount > 500) {
+
+        println("Adopt a Dog Today!")
+
+    } else {
+
+        println("Buy Cat Food At 20% Off")
+    }
+
 }
