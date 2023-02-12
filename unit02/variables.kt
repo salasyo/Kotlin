8f4a65e1fd0600de144eea03b6b453d59f2ccc6f
obed <122159877+salasyo@users.noreply.github.com>
2023-02-11 18:58:48 -0600

    Update variables.kt

--- unit02/variables.kt
+++ unit02/variables.kt
@@ -1,2 +1,17 @@
+fun main() {
 
-kadbduvbadnp;a
+    val myAge: Int = 19
+    var averageAge: Double = 19.00
+
+    val testNumber = 666
+    val evenOdd = testNumber
+
+    var answer = 0
+    ++answer
+
+    println(myAge)
+    println((averageAge + 30) / 2)
+    println(evenOdd)
+    println((answer + 10) * 10 * 1000)
+
+}
