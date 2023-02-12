10bbbb45e0bbcaac9126f0bdc6ddb85134fd46d6
obed <122159877+salasyo@users.noreply.github.com>
2023-02-11 19:38:01 -0600

    Create salesPrediction.kt

---
+++ unit02/salesPrediction.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val eastCoastSalesPercentage = 0.62
+    var companySales = 4600000
+    var eastCoastSales = eastCoastSalesPercentage * companySales
+
+
+    print("The East Coast division will generate $$eastCoastSales out of $$companySales total sales.")
+
+}
