a7bafc85edc13eebad07a0fea34c85acbc44718e
obed <122159877+salasyo@users.noreply.github.com>
2023-02-26 18:45:25 -0600

    Update restaurantSelector.kt

--- unit03/restaurantSelector.kt
+++ unit03/restaurantSelector.kt
@@ -1,7 +1,7 @@
 import java.util.Scanner
 
 fun main() {
-    // Define the restaurant options and their dietary information
+    
     val restaurants = mapOf(
         "Joe's Gourmet Burgers" to listOf("No", "No", "No"),
         "Main Street Pizza Company" to listOf("Yes", "No", "Yes"),
@@ -10,7 +10,6 @@ fun main() {
         "The Chef's Kitchen" to listOf("Yes", "Yes", "Yes")
     )
 
-    // Ask about dietary restrictions
     val scanner = Scanner(System.`in`)
     print("Is anyone in your party a vegetarian? ")
     val vegetarian = scanner.nextLine().equals("yes", ignoreCase = true)
@@ -19,14 +18,12 @@ fun main() {
     print("Is anyone in your party gluten-free? ")
     val glutenFree = scanner.nextLine().equals("yes", ignoreCase = true)
 
-    // Find restaurants that can accommodate the group's dietary needs
     val options = restaurants.filter { (name, dietary) ->
         (!vegetarian || dietary[0] == "Yes") &&
         (!vegan || dietary[1] == "Yes") &&
         (!glutenFree || dietary[2] == "Yes")
     }.keys
 
-    // Display the restaurant options
     println("\nHere are your restaurant choices:")
     for (restaurant in options) {
         println(restaurant)
